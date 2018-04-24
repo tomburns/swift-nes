44654823d912c3bb36dbc3415e8df07d2a3a7c95
Tom Burns <tomburns@gmail.com>
2018-04-24 11:41:32 -0500

    Initial Commit

---
+++ swift-nes/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-nes
+//
+//  Created by Tom Burns on 4/24/18.
+//  Copyright © 2018 Claptrap. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
